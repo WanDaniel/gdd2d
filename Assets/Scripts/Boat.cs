73c05199edb38e9b8846720802ac8a3919e7df69
BWRat <blake.ratliff.97@gmail.com>
2019-03-08 14:58:00 +0000

    Main menu placeholder
    Main menu scene added with non-functional buttons and cinematic background.

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+    public float rotationSpeed;
+    private Quaternion rotationA, rotationB;
+    private bool rotation;
+
+    void Awake()
+    {
+        if (rotationSpeed <= 0)
+            rotationSpeed = 1.5f;
+
+        rotationA = Quaternion.Euler(-3, -40, -2);
+        rotationB = Quaternion.Euler(2.5f, -40, 1.25f);
+    }
+
+    void Update()
+    {
+        Rock();
+    }
+
+    void Rock()
+    {
+        if (rotation)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationA, rotationSpeed * Time.deltaTime);
+
+            if (Quaternion.Angle(transform.rotation, rotationA) <= 1f)
+            {
+                rotation = false;
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationB, rotationSpeed * Time.deltaTime);
+
+            if (Quaternion.Angle(transform.rotation, rotationB) <= 1f)
+            {
+                rotation = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject howTo;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HowTo()
+    {
+        main.SetActive(false);
+        howTo.SetActive(true);
+    }
+
+    public void Back()
+    {
+        howTo.SetActive(false);
+        main.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
