2fce137031fd835a10c5333ce084f77636438e32
BWRat <blake.ratliff.97@gmail.com>
2019-03-08 15:41:57 +0000

    Main Menu
    Main menu with placeholders is complete.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,14 @@ public class MainMenu : MonoBehaviour
 {
     public GameObject main;
     public GameObject howTo;
+    public GameObject movementTip;
+    public GameObject cameraTip;
+    public GameObject puzzlesTip;
+
+    void Start()
+    {
+        Back();
+    }
 
     public void PlayGame()
     {
@@ -17,12 +25,34 @@ public class MainMenu : MonoBehaviour
     {
         main.SetActive(false);
         howTo.SetActive(true);
+        MovementTip();
     }
 
     public void Back()
     {
         howTo.SetActive(false);
         main.SetActive(true);
+    }
+
+    public void MovementTip()
+    {
+        movementTip.SetActive(true);
+        cameraTip.SetActive(false);
+        puzzlesTip.SetActive(false);
+    }
+
+    public void CameraTip()
+    {
+        movementTip.SetActive(false);
+        cameraTip.SetActive(true);
+        puzzlesTip.SetActive(false);
+    }
+
+    public void PuzzlesTip()
+    {
+        movementTip.SetActive(false);
+        cameraTip.SetActive(false);
+        puzzlesTip.SetActive(true);
     }
 
     public void QuitGame()
